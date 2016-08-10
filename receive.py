1c74b2e0bc07dd221cba7cb847d48bef84f874e7
lordokfey <yyyhhh125@163.com>
2016-08-10 17:02:54 +0800

    a

--- receive.py
+++ receive.py
@@ -152,7 +152,8 @@ def imgpro(classifier):
         img = img.reshape(height, width, 1)
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         img_in = cv2.resize(img, (227, 227))
-        sock.sendall(img_in)
+        data = aslist(img_in.data)
+        sock.sendall()
         m_rlt = ''
         if(0 == process_num % 2000):
             picFolder = str(process_num)
